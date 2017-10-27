9040fce81f06a1580177588482c843a24bf28995
ema <eusan91@hotmail.com>
2017-10-26 20:28:07 -0600

    fully functional changing language and MyApplication Application added

--- app/src/main/java/com/santamaria/mycurrentweather/Activities/MainActivity.kt
+++ app/src/main/java/com/santamaria/mycurrentweather/Activities/MainActivity.kt
@@ -4,18 +4,28 @@ import android.Manifest
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.location.Geocoder
 import android.location.LocationManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AlertDialog
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
+import com.santamaria.mycurrentweather.App.MyApplication
 import com.santamaria.mycurrentweather.Location.LocationAPI
 import com.santamaria.mycurrentweather.Retrofit.Forecast
 import com.santamaria.mycurrentweather.Models.Basic
@@ -27,7 +37,7 @@ import retrofit2.Response
 import java.util.*
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     var forecastData : Basic? = null
 
@@ -37,17 +47,30 @@ class MainActivity : AppCompatActivity(){
     var temperature : TextView? = null
     var high : TextView? = null
     var low : TextView? = null
+    var navDrawer : NavigationView? = null
+    var drawerLayout : DrawerLayout? = null
 
     var myLocation : LocationAPI? = null
     var locationManager : LocationManager? = null
     val GPS_PERMISSION = 1
 
+    val SPANISH_LANG = "es"
+    val ENGLISH_LANG = "en"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_drawer)
 
         getViews()
 
+        if (MyApplication.firstRun){
+            MyApplication.saveSetting(MyApplication.keyNameFirstRun)
+
+            drawerLayout?.openDrawer(Gravity.START)
+        }
+
+        navDrawer?.setNavigationItemSelectedListener ( this )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
 
@@ -56,7 +79,6 @@ class MainActivity : AppCompatActivity(){
                 requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), GPS_PERMISSION)
                 return
             }
-
         }
 
         myLocation = LocationAPI(this)
@@ -76,13 +98,8 @@ class MainActivity : AppCompatActivity(){
 
     fun getForecastInformation(latitude : Double, longitude : Double) {
 
-        var lang = Locale.getDefault().language
 
-        if (lang.compareTo("es", true) != 0 ){
-            lang = "en"
-        }
-
-        var forecastCall : Call<Basic> = Forecast.getForecast().getForecastApi(latitude,longitude, lang, "auto")
+        var forecastCall : Call<Basic> = Forecast.getForecast().getForecastApi(latitude,longitude, MyApplication.language, MyApplication.measurement)
 
         forecastCall.enqueue(object : Callback<Basic>{
             override fun onResponse(call: Call<Basic>?, response: Response<Basic>?) {
@@ -108,6 +125,8 @@ class MainActivity : AppCompatActivity(){
         high = findViewById(R.id.idTempH) as TextView
         low = findViewById(R.id.idTempL) as TextView
         location = findViewById(R.id.idLocation) as TextView
+        navDrawer = findViewById(R.id.idNavDrawer) as NavigationView
+        drawerLayout = findViewById(R.id.idDrawerLayout) as DrawerLayout
     }
 
     private fun loadMainActivity(latitude : Double, longitude : Double) {
@@ -168,8 +187,8 @@ class MainActivity : AppCompatActivity(){
         } else {
             myLocation = LocationAPI(this)
 
-            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1800000, 0f, myLocation)
-            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1800000, 0f, myLocation)
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1800000, 1000f, myLocation)
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1800000, 1000f, myLocation)
         }
 
     }
@@ -179,14 +198,14 @@ class MainActivity : AppCompatActivity(){
         var alert = AlertDialog.Builder(this).create()
         alert.setTitle(getString(R.string.alert_title))
         alert.setMessage(getString(R.string.alert_message))
-        alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", DialogInterface.OnClickListener { dialogInterface, i ->
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", { dialogInterface, i ->
 
             val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(settingsIntent)
 
         })
 
-        alert.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.button_negative), DialogInterface.OnClickListener { dialogInterface, i ->  })
+        alert.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.button_negative), { dialogInterface, i ->  })
 
         alert.show()
     }
@@ -199,8 +218,8 @@ class MainActivity : AppCompatActivity(){
         if (gpsEnabled!!) {
 
 
-            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1800000, 0f, myLocation)
-            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1800000, 0f, myLocation)
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1800000, 1000f, myLocation)
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1800000, 1000f, myLocation)
         }
     }
 
@@ -210,7 +229,94 @@ class MainActivity : AppCompatActivity(){
     }
 
     override fun onBackPressed() {
-        moveTaskToBack(true)
+        if (drawerLayout!!.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        } else {
+            moveTaskToBack(true)
+        }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+
+        when(item.itemId){
+
+            R.id.idExit -> { alertExitMessage()}
+            R.id.idMeasurement -> {}
+            R.id.idlanguage -> { createAlertDialogLanguage() }
+        }
+
+        drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun alertExitMessage() : Unit {
+
+        val alert = AlertDialog.Builder(this).create()
+
+        alert.setTitle(getString(R.string.exit))
+        alert.setMessage(getString(R.string.exit_title_msg))
+        alert.setButton(AlertDialog.BUTTON_NEGATIVE, "No", { dialogInterface, i -> })
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.yes), { dialogInterface, i ->
+            this.finish()
+        })
+        alert.show()
+    }
+
+    private fun createAlertDialogLanguage() {
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(getString(R.string.language))
+        builder.setIcon(R.drawable.ic_language)
+        builder.setMessage(getString(R.string.select_language))
+
+        val viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_define_language_item, null)
+        builder.setView(viewInflated)
+
+        var radioEnglish = viewInflated.findViewById(R.id.english) as RadioButton
+        var radioSpanish = viewInflated.findViewById(R.id.spanish) as RadioButton
+
+        if (MyApplication.language.compareTo("en") == 0) {
+            radioEnglish.isChecked = true
+        } else {
+            radioSpanish.isChecked = true
+        }
+
+        //get the one from the system and check
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            val englishState = radioEnglish.isChecked
+            val spanishState = radioSpanish.isChecked
+
+            if (englishState && MyApplication.language.compareTo(ENGLISH_LANG) != 0) {
+
+                MyApplication.language = ENGLISH_LANG
+                MyApplication.saveSetting(MyApplication.keyNameLanguage)
+                MyApplication.updateLanguage(resources)
+                restartApp()
+
+            } else if (spanishState && MyApplication.language.compareTo(SPANISH_LANG) != 0) {
+
+                MyApplication.language = SPANISH_LANG
+                MyApplication.saveSetting(MyApplication.keyNameLanguage)
+                MyApplication.updateLanguage(resources)
+                restartApp()
+
+            }
+        }.setNegativeButton(getString(R.string.cancel), { dialogInterface, i ->
+            //no action required
+        })
+
+        builder.create().show()
+    }
+
+    private fun restartApp() {
+        val intent1 = intent
+        intent1.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+        finish()
+        startActivity(intent1)
+    }
+
+
 }
 
---
+++ app/src/main/java/com/santamaria/mycurrentweather/App/MyApplication.kt
@@ -0,0 +1,82 @@
+package com.santamaria.mycurrentweather.App
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.view.Gravity
+import java.util.*
+
+/**
+ * Created by Santamaria on 26/10/2017.
+ */
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sharedPreferences = getSharedPreferences(SharedPreferencesName, Context.MODE_PRIVATE)
+
+        language = MyApplication.sharedPreferences!!.getString(MyApplication.keyNameLanguage, "en")
+        measurement = MyApplication.sharedPreferences!!.getString(MyApplication.keyNameMeasurement, "si")//us
+        firstRun = MyApplication.sharedPreferences!!.getBoolean(MyApplication.keyNameFirstRun, true)
+
+        if (firstRun){
+            language = getCellphoneLanguage()
+            saveSetting(MyApplication.keyNameLanguage)
+        }
+
+        updateLanguage(resources)
+
+    }
+
+    //SharedPreferences variables
+    companion object variables {
+        val SharedPreferencesName = "MyCurrentWeather_Settings"
+        val keyNameLanguage = "LANGUAGE"
+        val keyNameMeasurement = "MEASUREMENT"
+        val keyNameFirstRun = "FIRST_RUN"
+        var sharedPreferences: SharedPreferences? = null
+
+        var language = ""
+        var measurement = ""
+        var firstRun : Boolean = true
+
+        fun updateLanguage(resources : Resources){
+
+            val config = Configuration()
+            config.setLocale(Locale(language))
+            resources.updateConfiguration(config, resources.displayMetrics)
+
+        }
+
+        fun saveSetting(key: String) {
+
+            var editor = MyApplication.sharedPreferences!!.edit()
+
+            if (key == MyApplication.keyNameMeasurement){
+                editor.putString(key, measurement)
+            } else if (key == MyApplication.keyNameLanguage){
+                editor.putString(key, language)
+            } else if (firstRun && key == MyApplication.keyNameFirstRun){
+                firstRun = false
+                editor.putBoolean(key, false)
+            }
+
+            editor.commit()
+
+        }
+    }
+
+    private fun getCellphoneLanguage() : String{
+
+        var lang = Locale.getDefault().language
+
+        if (lang.compareTo("es", true) != 0 ){
+            lang = "en"
+        }
+
+        return lang
+    }
+}
